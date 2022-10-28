f252897c1a6465f05bcea54af20e4e9b3fdc8b2e
Serhii Vorotnoi <svorotnoy@gmail.com>
2022-10-28 16:40:16 +0300

    6.9

--- js/task-09.js
+++ js/task-09.js
@@ -14,5 +14,18 @@
 // }
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+const body = document.querySelector("body");
+
+changeColorBtn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+
+  color.textContent = randomColor;
+});
