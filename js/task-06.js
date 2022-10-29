ecf40b5c118ba2fab913a322c4ca634532414067
Serhii Vorotnoi <svorotnoy@gmail.com>
2022-10-29 16:22:40 +0300

    доработка 6.6

--- js/task-06.js
+++ js/task-06.js
@@ -4,11 +4,11 @@
 // Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
 
 document.getElementById("validation-input").onblur = function () {
-  if (this.getAttribute("data-length") > this.value.length) {
-    this.classList.remove("valid");
-    this.classList.add("invalid");
-  } else {
+  if (this.getAttribute("data-length") == this.value.length) {
     this.classList.remove("invalid");
     this.classList.add("valid");
+  } else {
+    this.classList.remove("valid");
+    this.classList.add("invalid");
   }
 };
